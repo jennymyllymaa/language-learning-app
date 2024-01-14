e13fe2b12bf20e41e684e94c2666bc1c2ed0addd
Jenny Myllymaa <jenny.i.myllymaa@tuni.fi>
2024-01-14 22:25:41 +0200

    Changed from route languages  to words because of the databasesstructure change.

---
+++ backend/routes/words.js
@@ -0,0 +1,26 @@
+const database = require("../database/repository");
+const express = require("express");
+
+const wordsRouter = express.Router();
+
+wordsRouter.get("/", async (req, res) => {
+  const languages = await database.returnAllWords();
+  res.json(languages);
+});
+
+wordsRouter.post("/", async (req, res) => {
+  const newWord = await database.saveWord(req.body);
+  res.status(201).json(newWord);
+});
+
+wordsRouter.delete("/:myId([0-9]+)", async (req, res) => {
+  const id = parseInt(req.params.myId);
+  try {
+    await database.deleteWordById(id);
+    res.status(204).end();
+  } catch {
+    res.status(404).end();
+  }
+});
+
+module.exports = wordsRouter;
