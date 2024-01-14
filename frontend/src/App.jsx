1c61a18f61d7ae56db9ac1b9218adff8ba31ec59
Jenny Myllymaa <jenny.i.myllymaa@tuni.fi>
2024-01-14 23:10:20 +0200

    frontend: Some test content done to ensure that backend and frontend work together.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,25 @@
 import { useState } from 'react'
 
 function App() {
+  const [words, setWords] = useState([]);
+
+
+  const testifetch = async () => {
+    let hr = await fetch(`${import.meta.env.VITE_API_URL}/api/words`);
+    let data = await hr.json();
+    console.log(data);
+    setWords(data);
+  };
+
+  let wordsArr = words.map((word) => <li key={word.id}>{word.english} - {word.finnish}</li>);
 
   return (
-    <p>Hello</p>
-  )
+    <div>
+      <p>Hello</p>
+      <div>{wordsArr}</div>
+      <button onClick={testifetch}>testi</button>
+    </div>
+  );
 }
 
 export default App
