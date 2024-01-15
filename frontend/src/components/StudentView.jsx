5179cdc7fab69082a6579d4097d4aee88c766ff8
Jenny Myllymaa <jenny.i.myllymaa@tuni.fi>
2024-01-15 15:46:20 +0200

    fronend/src/components/studentView.jsx: Outlining stucture.

--- frontend/src/components/StudentView.jsx
+++ frontend/src/components/StudentView.jsx
@@ -1,8 +1,20 @@
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import Prompt from "./Prompt.jsx"
+
 function StudentView() {
-  return(
-    <div>
-      <p>moi oppilaat</p>
-    </div>
+  return (
+    <Box>
+      <Grid container rowSpacing={1} columnSpacing={2}>
+        <Grid item xs={6}>
+          <Prompt />
+        </Grid>
+        <Grid item xs={6}>
+          <Paper>Vastaus</Paper>
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
 
