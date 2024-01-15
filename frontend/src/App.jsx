5afb5d081685367fc839eb442face1dda3ce785e
Jenny Myllymaa <jenny.i.myllymaa@tuni.fi>
2024-01-15 15:47:46 +0200

    frontends/src/App.jsx: Create needed states and some structure for the site.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,55 +1,68 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import StudentView from "./components/StudentView.jsx";
 import TeacherView from "./components/TeacherView.jsx";
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
-
+import Container from "@mui/material/Container";
 
 function App() {
+  //All words from the database
   const [words, setWords] = useState([]);
+  //Currently used language with {fromLanguage, toLanguage}
+  const [languages, setLanguages] = useState({});
+  //List of words in the current test with {question, answer}
+  const [practiseWords, setPractiseWords] = useState([]);
 
+  const propsToTeacherView = {
+    words,
+    practiseWords,
+    setPractiseWords,
+    languages,
+    setLanguages
+  };
 
-  const testifetch = async () => {
+  const propsToStudentView = {
+    languages,
+    practiseWords
+  };
+
+  //Function to fetch all words from backend and set them to words state
+  const fetchWords = async () => {
     let hr = await fetch(`${import.meta.env.VITE_API_URL}/api/words`);
     let data = await hr.json();
-    console.log(data);
     setWords(data);
   };
 
-  let wordsArr = words.map((word) => <li key={word.id}>{word.english} - {word.finnish}</li>);
+  //Use fetchWords on the first render
+  useEffect(() => {fetchWords()}, []);
 
   return (
     <BrowserRouter>
-      <div className="App">
-        <h1>Learn Languages</h1>
+      <Container maxWidth="md">
+        <div className="App">
+          <h1>Learn Languages</h1>
 
-        <div className="mainNavigation">
-          <div>
-            <Link to="/">
-              <button>Student</button>
-            </Link>
-            <Link to="teacher">
-              <button>Teacher</button>
-            </Link>
-          </div>
-        </div>
-
-          <div>
-            <p>Hello</p>
-            <div>{wordsArr}</div>
-            <button onClick={testifetch}>testi</button>
+          <div className="mainNavigation">
+            <div>
+              <Link to="/">
+                <button>Student</button>
+              </Link>
+              <Link to="teacher">
+                <button>Teacher</button>
+              </Link>
+            </div>
           </div>
 
           <div>
             <Routes>
-              <Route path="/" element={<StudentView />} />
-              <Route path="/teacher" element={<TeacherView />} />
+              <Route path="/" element={<StudentView {...propsToStudentView} />} />
+              <Route
+                path="/teacher"
+                element={<TeacherView {...propsToTeacherView} />}
+              />
             </Routes>
           </div>
         </div>
+      </Container>
     </BrowserRouter>
   );
 }
