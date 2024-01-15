0fd95fa5e9fa4fe21cfc2f31c035f08b6fb3cc48
Jenny Myllymaa <jenny.i.myllymaa@tuni.fi>
2024-01-15 23:10:07 +0200

    frontend/src/components/TeacherView.js: Add states for language and word availibility.

--- frontend/src/components/TeacherView.jsx
+++ frontend/src/components/TeacherView.jsx
@@ -1,13 +1,19 @@
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
+import { useState } from 'react';
 
-function TeacherView() {
+function TeacherView(props) {
+  //State that holdds the available words pairs for selected languages as {question, answer}
+  const [availableWords, setAvailableWords] = useState([]);
+
+  //Function that uses languages to fetch available wordpairs from backend and set those to availableWords
+
   return (
     <div>
       <Box>
         <button>Choose languages</button>
-        <button>Coose words</button>
+        <button>Choose words</button>
         <ul>{props.practiseWords}</ul>
         <button>Save</button>
       </Box>
