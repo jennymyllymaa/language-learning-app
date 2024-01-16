c104e930e2ab1f67b4f9cbf061f57c4e0ec6a220
Jenny Myllymaa <jenny.i.myllymaa@tuni.fi>
2024-01-16 22:53:49 +0200

    Add file CurrentTest with only base structure.

---
+++ frontend/src/components/CurrentTest.jsx
@@ -0,0 +1,12 @@
+import Grid from "@mui/material/Grid";
+
+function CurrentTest(props) {
+  return (
+    <Grid
+      container
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+    ></Grid>
+  );
+}
