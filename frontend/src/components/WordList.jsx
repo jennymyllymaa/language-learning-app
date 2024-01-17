4b225b80fd4d657faeebea12a3af42f1d3fc37e6
Jenny Myllymaa <jenny.i.myllymaa@tuni.fi>
2024-01-17 19:59:34 +0200

    Make table/datagrid editable.

--- frontend/src/components/WordList.jsx
+++ frontend/src/components/WordList.jsx
@@ -4,18 +4,20 @@ import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
-import { Typography } from "@mui/material";
+import { TextareaAutosize, Typography } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
 
 function WordList(props) {
   const columns = [
-    { field: "id", headerName: "Id", width: 60 },
-    { field: "tag", headerName: "Tag", width: 130 },
-    { field: "english", headerName: "English", width: 130 },
-    { field: "finnish", headerName: "Finnish", width: 130 },
-    { field: "swedish", headerName: "Swedish", width: 130 },
-    { field: "german", headerName: "German", width: 130 },
-    { field: "italian", headerName: "Italian", width: 130 },
+    { field: "id", headerName: "Id", width: 100 },
+    { field: "tag", headerName: "Tag", width: 130, editable: true },
+    { field: "english", headerName: "English", width: 130, editable: true },
+    { field: "finnish", headerName: "Finnish", width: 130, editable: true },
+    { field: "swedish", headerName: "Swedish", width: 130, editable: true },
+    { field: "german", headerName: "German", width: 130, editable: true },
+    { field: "italian", headerName: "Italian", width: 130, editable: true },
   ];
 
   const rows = props.words.map((word) => ({
@@ -28,6 +30,14 @@ function WordList(props) {
     italian: word.italian,
   }));
 
+  const mySaveOnServerFunction = (updatedRow) => {
+    console.log(updatedRow);
+  };
+
+  const handleProcessRowUpdateError = (parametri) => {
+    console.log(parametri);
+  };
+
   return (
     <Grid
       container
@@ -35,20 +45,36 @@ function WordList(props) {
       justifyContent="center"
       alignItems="center"
     >
-      <Typography variant="h5" sx={{margin: "20px"}}>Words</Typography>
-      <div style={{ height: 400, width: "100%" }}>
+      <Typography variant="h5" sx={{ margin: "20px" }}>
+        Words
+      </Typography>
+      <div style={{ height: 500, width: "100%" }}>
         <DataGrid
           rows={rows}
           columns={columns}
+          getRowId={(row) => row.id} // Provide a custom getRowId function
           initialState={{
             pagination: {
-              paginationModel: { page: 0, pageSize: 5 },
+              paginationModel: { page: 0, pageSize: 10 },
             },
           }}
-          pageSizeOptions={[5, 10]}
-          checkboxSelection
+          pageSizeOptions={[10, 50]}
+          hideFooterSelectedRowCount
+          processRowUpdate={(updatedRow, originalRow) =>
+            mySaveOnServerFunction(updatedRow)
+          }
+          onProcessRowUpdateError={(error) =>
+            handleProcessRowUpdateError(error)
+          }
         />
       </div>
+      <Box>
+        <Button>Update</Button>
+        <Button>Delete</Button>
+      </Box>
+      <Box>
+        <Button>Add word</Button>
+      </Box>
     </Grid>
   );
 }
